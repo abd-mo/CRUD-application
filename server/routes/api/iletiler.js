2e9c0bfd95480b2c1e2f423616ead321665871c3
abdelrahman mohamed <abdelrahman.ise@gmail.com>
2019-12-06 04:33:12 -0800

    delete database code

--- server/routes/api/iletiler.js
+++ server/routes/api/iletiler.js
@@ -3,32 +3,10 @@ const mongodb=require('mongodb');
 const router=express.Router();
 
 
-//router.get('/',(req,res)=>{
-   // res.send('hello to my web site')
-//});
-
-router.get('/',async(req,res)=>
-{
-    const iletiler=await loadIletilerCollection();
-res.send(await iletiler.find({}).toArray());
+router.get('/',(req,res)=>{
+    res.send('hello to my web site')
 });
-//İletiler koleksiyonunu çekmek için
-async function loadIletilerCollection()
-{
 
-    const client=await mongodb.MongoClient.connect
-    (
 
-        'mongodb+srv://abdelrahman:<password>@cluster0-fqdy1.mongodb.net/test?retryWrites=true&w=majority' ,
-        {
-
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-        }
-
-    );
- 
-    return client.db('deneme').collection('iletiler');
-}
 module.exports=router;
 
