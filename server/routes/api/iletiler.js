edc89187553b9f4b2cb74d9ed191a81f18ecee3f
abdelrahman mohamed <abdelrahman.ise@gmail.com>
2019-12-06 03:10:39 -0800

    mongodb1

--- server/routes/api/iletiler.js
+++ server/routes/api/iletiler.js
@@ -6,5 +6,26 @@ const router=express.Router();
 router.get('/',(req,res)=>{
     res.send('hello to my web site')
 });
+router.get('/',async(req,res)=>
+{
+    const iletiler=await loadIletilerCollection();
+res.send(await iletiler.find({}).toArray());
+});
+async function loadIletilerCollection(){
 
+    const client=await mongodb.MongoClient.connect
+    (
+
+        'mongodb+srv://abdelrahman:<password>@cluster0-fqdy1.mongodb.net/test?retryWrites=true&w=majority' ,
+        {
+
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+        }
+
+    );
+ 
+    return client.db('deneme').collection('iletiler');
+}
 module.exports=router;
+
