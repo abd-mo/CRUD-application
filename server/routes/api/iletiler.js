3d3e5cedb1e6db5f4618a2051be54694840ed11b
abdelrahman mohamed <abdelrahman.ise@gmail.com>
2019-12-06 04:38:24 -0800

    add database code

--- server/routes/api/iletiler.js
+++ server/routes/api/iletiler.js
@@ -7,6 +7,28 @@ router.get('/',(req,res)=>{
     res.send('hello to my web site')
 });
 
+router.get('/',async(req,res)=>
+{
+    const iletiler=await loadIletilerCollection();
+res.send(await iletiler.find({}).toArray());
+});
+//İletiler koleksiyonunu çekmek için
+async function loadIletilerCollection()
+{
 
+    const client=await mongodb.MongoClient.connect
+    (
+
+        'mongodb+srv://abdelrahman:<password>@cluster0-fqdy1.mongodb.net/test?retryWrites=true&w=majority' ,
+        {
+
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+        }
+
+    );
+ 
+    return client.db('deneme').collection('iletiler');
+}
 module.exports=router;
 
